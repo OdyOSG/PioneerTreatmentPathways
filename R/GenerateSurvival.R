4a2f92dbed005c76ae40f7059c273c6deecaaca8
tsemharb <gorbachevart@gmail.com>
2022-12-10 03:57:40 +0300

    improve time to event logic

--- R/GenerateSurvival.R
+++ R/GenerateSurvival.R
@@ -7,28 +7,30 @@ generateSurvival <- function(connection,
                              databaseId,
                              packageName) {
   eventIds <- events %>% dplyr::pull(eventId)
-  purrr::map_df(targetIds, function(targetId){
-    purrr::map_df(eventIds, function(.eventId){
-      outcomeIds <- events %>% 
-        dplyr::filter(eventId == .eventId) %>% 
-        dplyr::pull(outcomeCohortIds)
-      sql <- SqlRender::loadRenderTranslateSql(dbms = connection@dbms,
-                                               sqlFilename = "TimeToEvent.sql",
-                                               packageName = packageName,
-                                               warnOnMissingParameters = TRUE,
-                                               cohort_database_schema = cohortDatabaseSchema,
-                                               cohort_table = cohortTable,
-                                               outcome_ids = outcomeIds, 
-                                               target_id = targetId)
-      km <- DatabaseConnector::querySql(connection, sql, snakeCaseToCamelCase = T)
+  purrr::map_df(eventIds, function(.eventId){
+    outcomeIds <- events %>% 
+      dplyr::filter(eventId == .eventId) %>% 
+      dplyr::pull(outcomeCohortIds)
+    sql <- SqlRender::loadRenderTranslateSql(dbms = connection@dbms,
+                                             sqlFilename = "TimeToEvent.sql",
+                                             packageName = packageName,
+                                             warnOnMissingParameters = TRUE,
+                                             cohort_database_schema = cohortDatabaseSchema,
+                                             cohort_table = cohortTable,
+                                             outcome_ids = outcomeIds, 
+                                             target_ids = paste(targetIds, collapse = ', '))
+    km_grouped <- DatabaseConnector::querySql(connection, sql, snakeCaseToCamelCase = T)
+    
+    purrr::map_df(targetIds, function(.targetId){
       
-      ## edit
+      km <- km_grouped %>% filter(targetId == .targetId)
+      
       if (nrow(km) < 100 | length(km$event[km$event == 1]) < 1) {return(NULL)}
-      
+
       # TODO: Change to Cyclops
       surv_info <- survival::survfit(survival::Surv(timeToEvent, event) ~ 1, data = km)
       surv_info <- survminer::surv_summary(surv_info)
-      data.frame(targetId = targetId, eventId = .eventId, time = surv_info$time, surv = surv_info$surv, 
+      data.frame(targetId = .targetId, eventId = .eventId, time = surv_info$time, surv = surv_info$surv, 
                  n.censor = surv_info$n.censor, n.event = surv_info$n.event, n.risk = surv_info$n.risk,
                  lower = surv_info$lower, upper = surv_info$upper, databaseId = databaseId)
     })
--- inst/sql/sql_server/TimeToEvent.sql
+++ inst/sql/sql_server/TimeToEvent.sql
@@ -1,8 +1,9 @@
-SELECT row_number() OVER () AS id,
+SELECT cohort_definition_id AS target_id, 
+       row_number() OVER (PARTITION BY cohort_definition_id) AS id,
        DATEDIFF(day, cohort_start_date, event_date) AS time_to_event,
        event
 FROM (
-     SELECT t.cohort_start_date,
+     SELECT t.cohort_definition_id, t.cohort_start_date,
             coalesce(min(o.cohort_start_date), max(t.cohort_end_date)) AS event_date,
             CASE WHEN min(o.cohort_start_date) IS NULL THEN 0 ELSE 1 END AS event
      FROM @cohort_database_schema.@cohort_table t
@@ -15,6 +16,6 @@ FROM (
          ON t.subject_id = o.subject_id
             AND o.cohort_start_date >= t.cohort_start_date
             AND o.cohort_start_date <= t.cohort_end_date
-     WHERE t.cohort_definition_id = @target_id
-     GROUP BY t.subject_id, t.cohort_start_date
+     WHERE t.cohort_definition_id IN (@target_ids)
+     GROUP BY t.cohort_definition_id, t.subject_id, t.cohort_start_date
      ) tab;
