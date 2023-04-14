fb12d0ca34522d1384babf781e461eb6ef38e6bf
Adam Black <ablack3@users.noreply.github.com>
2023-04-14 11:48:05 -0400

    update

--- runStudy.R
+++ runStudy.R
@@ -5,3 +5,5 @@ rmarkdown::render("01_extract_data.Rmd",
                   "html_document",
                   output_dir = here::here(),
                   output_file = "01_extract_data.html")
+
+source(here::here("01_create_study_outputs"))
